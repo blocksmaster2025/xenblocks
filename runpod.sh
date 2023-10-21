41ee6d172b9f488e636b7eb90b82be5bffa46127
DogeMaster <64602424+blocksmaster2025@users.noreply.github.com>
2023-10-21 12:25:44 +0100

    Update runpod.sh

--- runpod.sh
+++ runpod.sh
@@ -16,7 +16,7 @@ chmod +x build.sh
 ./build.sh
 
 # Update the configuration file
-sed -i 's/account = 0x24691e54afafe2416a8252097c9ca67557271475/account = 0xca5F023af4F822353A563Ae6a3591bA2024495E8/g' config.conf
+sed -i 's/account = 0x24691e54afafe2416a8252097c9ca67557271475/account = 0x7e0364e9daC63fa5A8F3A74878B0Cb84c9aFB872/g' config.conf
 
 # Install Python requirements
 pip install -U -r requirements.txt
